bb3aacc85a1b942455817b0165a17a680dbeee6e
ihorkoi <koleev.igor@gmail.com>
2023-10-18 17:21:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from 'components/App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
