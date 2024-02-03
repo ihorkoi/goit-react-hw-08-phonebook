163b50efa656877789540eee9347bbf02bafa016
ihorkoi <koleev.igor@gmail.com>
2024-02-03 17:29:35 +0200

    rename api

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://phonebook-csua.onrender.com/';
+axios.defaults.baseURL = 'https://phonebook-csua.onrender.com/api/';
 
 const setToken = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
@@ -14,7 +14,7 @@ export const registerUser = createAsyncThunk(
   'auth/registerUser',
   async (user, thunkAPI) => {
     try {
-      const response = await axios.post('/users/signup', user);
+      const response = await axios.post('/auth/signup', user);
       setToken(response.data.token);
       return response.data;
     } catch (error) {
@@ -33,7 +33,7 @@ export const refreshUser = createAsyncThunk(
     }
     try {
       setToken(persistedToken);
-      const response = await axios.get('/users/current');
+      const response = await axios.get('/auth/current');
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -42,7 +42,7 @@ export const refreshUser = createAsyncThunk(
 );
 export const logIn = createAsyncThunk('auth/logIn', async (user, thunkAPI) => {
   try {
-    const response = await axios.post('/users/login', user);
+    const response = await axios.post('/auth/login', user);
     setToken(response.data.token);
     return response.data;
   } catch (error) {
@@ -51,7 +51,7 @@ export const logIn = createAsyncThunk('auth/logIn', async (user, thunkAPI) => {
 });
 export const logOut = createAsyncThunk('auth/logOut', async (_, thunkAPI) => {
   try {
-    await axios.post('/users/logout');
+    await axios.post('/auth/logout');
     clearToken();
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://phonebook-csua.onrender.com/';
+axios.defaults.baseURL = 'https://phonebook-csua.onrender.com/api';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
